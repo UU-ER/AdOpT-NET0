2d6496688a9bc0359840e5c86d89d6fe36669994
Wiegner <j.f.wiegner@uu.nl>
2022-11-22 14:10:59 +0100

    Add todo

--- main.py
+++ main.py
@@ -5,6 +5,7 @@
 # TODO: Implement length of time step
 # TODO: Implement design days (retain extremes)
 # TODO: Implement Lukas Algorithm
+# TODO: bla
 from src.data_management.data_handling import DataHandle, load_data_handle
 from src.data_management.create_templates import create_empty_network_data
 import pandas as pd
