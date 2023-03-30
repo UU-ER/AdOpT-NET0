11e2b3c875d70978310f858ff34c0a190b6ea327
6574114 <j.f.wiegner@uu.nl>
2023-03-30 14:33:10 +0200

    Monte carlo docu updated

--- src/model_configuration.py
+++ src/model_configuration.py
@@ -8,67 +8,65 @@ class ModelConfiguration:
 
     List of optimization settings that can be specified:
 
+    +------------------+----------------------------------------------+---------------------------------------------+---------+
+    | Name             | Definition                                   | Options                                     | Default |
+    +------------------+----------------------------------------------+---------------------------------------------+---------+
+    | objective        | String specifying the objective/type         | 'costs', 'emissions_pos', 'emissions_neg',  | 'costs' |
+    |                  | of optimization                              | 'emissions_minC', 'pareto'                  |         |
+    +------------------+----------------------------------------------+---------------------------------------------+---------+
+    | montecarlo.range | Value defining the range in which variables  |                                             | 0.2     |
+    |                  | are varied in Monte Carlo simulations        |                                             |         |
+    +------------------+----------------------------------------------+---------------------------------------------+---------+
+    | montecarlo.N     | Number of Monte Carlo simulations            |                                             | 100     |
+    +------------------+----------------------------------------------+---------------------------------------------+---------+
+    | pareto.N         | Number of Pareto points                      |                                             | 5       |
+    +------------------+----------------------------------------------+---------------------------------------------+---------+
+    | timestaging      | Switch to turn timestaging on/off            | {0,1}                                       | 0       |
+    +------------------+----------------------------------------------+---------------------------------------------+---------+
+    | techstaging      | Switch to turn tecstaging on/off             | {0,1}                                       | 0       |
+    +------------------+----------------------------------------------+---------------------------------------------+---------+
 
-+------------------+----------------------------------------------+---------------------------------------------+---------+
-| Name             | Definition                                   | Options                                     | Default |
-+------------------+----------------------------------------------+---------------------------------------------+---------+
-| objective        | String specifying the objective/type         | 'costs', 'emissions_pos', 'emissions_neg',  | 'costs' |
-|                  | of optimization                              | 'emissions_minC', 'pareto'                  |         |
-+------------------+----------------------------------------------+---------------------------------------------+---------+
-| montecarlo.range | Value defining the range in which variables  |                                             | 0.2     |
-|                  | are varied in Monte Carlo simulations        |                                             |         |
-+------------------+----------------------------------------------+---------------------------------------------+---------+
-| montecarlo.N     | Number of Monte Carlo simulations            |                                             | 100     |
-+------------------+----------------------------------------------+---------------------------------------------+---------+
-| pareto.N         | Number of Pareto points                      |                                             | 5       |
-+------------------+----------------------------------------------+---------------------------------------------+---------+
-| timestaging      | Switch to turn timestaging on/off            | {0,1}                                       | 0       |
-+------------------+----------------------------------------------+---------------------------------------------+---------+
-| techstaging      | Switch to turn tecstaging on/off             | {0,1}                                       | 0       |
-+------------------+----------------------------------------------+---------------------------------------------+---------+
+    List of solver settings that can be specified:
 
-List of solver settings that can be specified:
+    +---------+-------------------------------------+----------+----------+
+    | Name    | Definition                          | Options  | Default  |
+    +---------+-------------------------------------+----------+----------+
+    | solver  | String specifying the solver used   | 'gurobi' | 'gurobi' |
+    +---------+-------------------------------------+----------+----------+
+    | mipgap  | Value to define MIP gap             |          | 0.01     |
+    +---------+-------------------------------------+----------+----------+
+    | timelim | Value to define time limit in hours |          | 10       |
+    +---------+-------------------------------------+----------+----------+
 
-+---------+-------------------------------------+----------+----------+
-| Name    | Definition                          | Options  | Default  |
-+---------+-------------------------------------+----------+----------+
-| solver  | String specifying the solver used   | 'gurobi' | 'gurobi' |
-+---------+-------------------------------------+----------+----------+
-| mipgap  | Value to define MIP gap             |          | 0.01     |
-+---------+-------------------------------------+----------+----------+
-| timelim | Value to define time limit in hours |          | 10       |
-+---------+-------------------------------------+----------+----------+
+    List of energy balance settings that can be specified:
 
-List of energy balance settings that can be specified:
+    +-------------+--------------------------------------------------+---------+---------+
+    | Name        | Definition                                       | Options | Default |
+    +-------------+--------------------------------------------------+---------+---------+
+    | violation   | Determines if the energy balance can be violated | {0,1}   | 0       |
+    +-------------+--------------------------------------------------+---------+---------+
+    | copperplate | Determines if a copperplate approach is used     | {0,1}   | 0       |
+    +-------------+--------------------------------------------------+---------+---------+
 
-+-------------+--------------------------------------------------+---------+---------+
-| Name        | Definition                                       | Options | Default |
-+-------------+--------------------------------------------------+---------+---------+
-| violation   | Determines if the energy balance can be violated | {0,1}   | 0       |
-+-------------+--------------------------------------------------+---------+---------+
-| copperplate | Determines if a copperplate approach is used     | {0,1}   | 0       |
-+-------------+--------------------------------------------------+---------+---------+
+    List of economic settings that can be specified:
 
-List of economic settings that can be specified:
+    +----------------+----------------------------------------------+---------+---------+
+    | Name           | Definition                                   | Options | Default |
+    +----------------+----------------------------------------------+---------+---------+
+    | globalinterest | Determines if a global interest rate is used | {0,1}   | 0       |
+    +----------------+----------------------------------------------+---------+---------+
+    | globalcosttype | Determines if a global cost function is used | {0,1}   | 0       |
+    +----------------+----------------------------------------------+---------+---------+
 
-+----------------+----------------------------------------------+---------+---------+
-| Name           | Definition                                   | Options | Default |
-+----------------+----------------------------------------------+---------+---------+
-| globalinterest | Determines if a global interest rate is used | {0,1}   | 0       |
-+----------------+----------------------------------------------+---------+---------+
-| globalcosttype | Determines if a global cost function is used | {0,1}   | 0       |
-+----------------+----------------------------------------------+---------+---------+
+    List of technology and network performance settings that can be specified:
 
-List of technology and network performance settings that can be specified:
-
-+----------------------+------------------------------------------------+---------+---------+
-| Name                 | Definition                                     | Options | Default |
-+----------------------+------------------------------------------------+---------+---------+
-| globalconversiontype | Determines if a global conversion type is used | {0,1}   | 0       |
-+----------------------+------------------------------------------------+---------+---------+
-| dynamics             | Determines if dynamics are used                | {0,1}   | 0       |
-+----------------------+------------------------------------------------+---------+---------+
-
+    +----------------------+------------------------------------------------+---------+---------+
+    | Name                 | Definition                                     | Options | Default |
+    +----------------------+------------------------------------------------+---------+---------+
+    | globalconversiontype | Determines if a global conversion type is used | {0,1}   | 0       |
+    +----------------------+------------------------------------------------+---------+---------+
+    | dynamics             | Determines if dynamics are used                | {0,1}   | 0       |
+    +----------------------+------------------------------------------------+---------+---------+
     """
 
     def __init__(self):
@@ -106,6 +104,9 @@ List of technology and network performance settings that can be specified:
         """
         Function to define the range within the variables are varied (+/-) and the number of simulations
         for the Monte Carlo simulation.
+
+        :param float range: SD with which investment cost is varied
+        :param int N: number of simulations
         """
 
         self.optimization.montecarlo.range = range
