1f6ae799592ddcc9688b27d2dbf31955da31a110
Tiggeloven <j.l.tiggeloven@uu.nl>
2023-09-25 14:54:52 +0200

    include perf function 4

--- src/data_management/components/utilities.py
+++ src/data_management/components/utilities.py
@@ -72,6 +72,8 @@ def fit_performance_generic_tecs(tec_data, time_steps):
         fitting = FitGenericTecTypeType2(tec_data)
     elif performance_function_type == 3:
         fitting = FitGenericTecTypeType3(tec_data)
+    elif performance_function_type == 4:
+        fitting = FitGenericTecTypeType3(tec_data)
     else:
         raise Exception("performance_function_type must be an integer between 1 and 3")
     fitting.fit_performance_function(performance_data)
@@ -247,6 +249,75 @@ class FitGenericTecTypeType2(FittedPerformance):
 class FitGenericTecTypeType3(FittedPerformance):
     """
     Subclass to fit performance of type3 performance functions (piecewise linear, with min partload)
+    out = alpha1[i] * in + alpha2
+    """
+
+    def fit_performance_function(self, performance_data):
+        """
+        Fits performance function for input-output data for type 2 technologies
+
+        :param performance_data: performance data
+        :return: coefficients
+        """
+        if 'nr_segments_piecewise' in performance_data:
+            nr_seg = performance_data['nr_segments_piecewise']
+        else:
+            nr_seg = 2
+        x = performance_data['in']
+        y = performance_data['out']
+        self.coefficients = fit_piecewise_function(x, y, nr_seg)
+
+    def calculate_input_bounds(self, size_based_on, time_steps):
+        """
+        Calculates input bounds for type 1 generic technologies
+
+        :param str size_based_on: 'input' or 'output'
+        :return: input bounds
+        """
+        if size_based_on == 'input':
+            for car in self.input_carrier:
+                self.bounds['input'][car] = np.column_stack((np.zeros(shape=(time_steps)),
+                                                             np.ones(shape=(time_steps))))
+        elif size_based_on == 'output':
+            for car in self.input_carrier:
+                if car in self.coefficients:
+                    car_aux = car
+                else:
+                    car_aux = 'out'
+                self.bounds['input'][car] = (np.column_stack((np.zeros(shape=time_steps),
+                                                              np.ones(shape=time_steps))) -
+                                             self.coefficients[car_aux]['alpha2'][-1]) / \
+                                            self.coefficients[car_aux]['alpha1'][-1]
+        else:
+            raise Exception('size_based_on must be either input or output')
+
+    def calculate_output_bounds(self, size_based_on, time_steps):
+        """
+        Calculates output bounds for type 1 generic technologies
+
+        :param str size_based_on: 'input' or 'output'
+        :return: output bounds
+        """
+        if size_based_on == 'input':
+            for car in self.output_carrier:
+                if car in self.coefficients:
+                    car_aux = car
+                else:
+                    car_aux = 'out'
+                self.bounds['output'][car] = np.column_stack((np.zeros(shape=(time_steps)),
+                                                              np.ones(shape=(time_steps)) *
+                                                              self.coefficients[car_aux]['alpha1'][-1] + \
+                                                              self.coefficients[car_aux]['alpha2'][-1]))
+        elif size_based_on == 'output':
+            for car in self.output_carrier:
+                self.bounds['output'][car] = np.column_stack((np.zeros(shape=(time_steps)),
+                                                              np.ones(shape=(time_steps))))
+        else:
+            raise Exception('size_based_on must be either input or output')
+
+class FitGenericTecTypeType4(FittedPerformance):
+    """
+    Subclass to fit performance of type4 performance functions (piecewise linear, with min partload)
     out = alpha1[i] * in + alpha2
     """
 
