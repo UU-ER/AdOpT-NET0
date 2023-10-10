Fixing bugs in scaling (Pyomo related, dict keys cannot be used for initializing sets, at least not in conjunction with scaling)

main.py
@@ -12,41 +12,52 @@ data_save_path = Path('./user_data/data_handle_test')
 
 # TOPOLOGY
 topology = dm.SystemTopology()
-topology.define_time_horizon(year=2001,start_date='01-01 00:00', end_date='01-03 01:00', resolution=1)
+topology.define_time_horizon(year=2001,start_date='01-01 00:00', end_date='12-31 23:00', resolution=1)
 topology.define_carriers(['electricity', 'gas', 'hydrogen', 'heat'])
-topology.define_nodes(['onshore'])
-# topology.define_nodes(['onshore', 'offshore'])
-# topology.define_new_technologies('onshore', ['Storage_Battery'])
-# topology.define_new_technologies('onshore', ['Photovoltaic', 'Storage_Battery', 'WindTurbine_Onshore_4000'])
-# topology.define_new_technologies('onshore', ['testCONV4_1'])
-# topology.define_new_technologies('onshore', ['GasTurbine_simple'])
-# topology.define_new_technologies('onshore', ['TestPumpedHydro_Open'])
+# topology.define_nodes(['onshore'])
+topology.define_nodes(['onshore', 'offshore'])
+topology.define_new_technologies('onshore', ['Photovoltaic', 'Storage_Battery', 'WindTurbine_Onshore_4000',
+                                             'GasTurbine_simple'])
+topology.define_new_technologies('offshore', ['WindTurbine_Offshore_6000'])
 
-# distance = dm.create_empty_network_matrix(topology.nodes)
-# distance.at['onshore', 'offshore'] = 1
-# distance.at['offshore', 'onshore'] = 1
-#
-# connection = dm.create_empty_network_matrix(topology.nodes)
-# connection.at['onshore', 'offshore'] = 1
-# connection.at['offshore', 'onshore'] = 1
-# topology.define_new_network('electricitySimple', distance=distance, connections=connection)
+distance = dm.create_empty_network_matrix(topology.nodes)
+distance.at['onshore', 'offshore'] = 100
+distance.at['offshore', 'onshore'] = 100
+
+connection = dm.create_empty_network_matrix(topology.nodes)
+connection.at['onshore', 'offshore'] = 1
+connection.at['offshore', 'onshore'] = 1
+topology.define_new_network('electricitySimple', distance=distance, connections=connection)
 
 # Initialize instance of DataHandle
 data = dm.DataHandle(topology)
 
 # CLIMATE DATA
-from_file = 1
+from_file = 0
 if from_file == 1:
     data.read_climate_data_from_file('onshore', './data/climate_data_onshore.txt')
-    # data.read_climate_data_from_file('offshore', './data/climate_data_offshore.txt')
+    data.read_climate_data_from_file('offshore', './data/climate_data_offshore.txt')
+else:
+    lat = 52
+    lon = 5.16
+    data.read_climate_data_from_api('onshore', lon, lat, save_path='./data/climate_data_onshore.txt')
+    lat = 52.2
+    lon = 4.4
+    data.read_climate_data_from_api('offshore', lon, lat, save_path='./data/climate_data_offshore.txt')
 
-# inflow = np.ones(len(topology.timesteps)) * 1000
-# data.read_hydro_natural_inflow('onshore', 'TestPumpedHydro_Open', inflow)
 
 # DEMAND
 electricity_demand = np.ones(len(topology.timesteps)) * 1000
 data.read_demand_data('onshore', 'electricity', electricity_demand)
-data.read_import_limit_data('onshore', 'electricity', electricity_demand)
+
+import_lim = np.ones(len(topology.timesteps)) * 100
+data.read_import_limit_data('onshore', 'electricity', import_lim)
+gas_import = np.ones(len(topology.timesteps)) * 2000
+data.read_import_limit_data('onshore', 'gas', gas_import)
+
+import_lim = np.ones(len(topology.timesteps)) * 10000
+data.read_export_limit_data('onshore', 'heat', import_lim)
+
 data.read_import_price_data('onshore', 'electricity', np.ones(len(topology.timesteps)) * 60)
 data.read_import_emissionfactor_data('onshore', 'electricity', np.ones(len(data.topology.timesteps)) * 0.1)
 # production_prof = np.ones(len(topology.timesteps)) * 1000
@@ -62,12 +73,10 @@ data.read_import_emissionfactor_data('onshore', 'electricity', np.ones(len(data.
 # co2 = np.ones(len(topology.timesteps)) * 10000/8760
 # data.read_demand_data('onshore', 'CO2', co2)
 
-# IMPORT
-# gas_import = np.ones(len(topology.timesteps)) * 1000
-# data.read_import_limit_data('onshore', 'electricity', gas_import)
-#
-# gas_price = np.ones(len(topology.timesteps)) * 1000
-# data.read_import_price_data('onshore', 'gas', gas_price)
+
+
+gas_price = np.ones(len(topology.timesteps)) * 70
+data.read_import_price_data('onshore', 'gas', gas_price)
 
 # READ TECHNOLOGY AND NETWORK DATA
 
@@ -77,6 +86,15 @@ data.read_network_data()
 
 # SAVING/LOADING DATA FILE
 configuration = ModelConfiguration()
+configuration.scaling = 0
+
+# # Read data
+energyhub = EnergyHub(data, configuration)
+energyhub.quick_solve()
+
+# SAVING/LOADING DATA FILE
+configuration = ModelConfiguration()
+configuration.scaling = 1
 
 # # Read data
 energyhub = EnergyHub(data, configuration)