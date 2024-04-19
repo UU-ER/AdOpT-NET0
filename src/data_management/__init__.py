7ebe1dad70b03ca9bde966564ebb5e0b8d737be1
6574114 <j.f.wiegner@uu.nl>
2024-04-19 14:56:29 +0200

    Input data folder and all other required things

--- src/data_management/__init__.py
+++ src/data_management/__init__.py
@@ -1,3 +1,9 @@
 from .handle_topology import SystemTopology, create_empty_network_matrix
 from .handle_input_data import DataHandle, ClusteredDataHandle, DataHandle_AveragedData
-from .utilities import load_object, save_object
+from .utilities import (
+    load_object,
+    save_object,
+    create_input_data_folder_template,
+    create_optimization_templates,
+    check_input_data_consistency,
+)
--- src/data_management/utilities.py
+++ src/data_management/utilities.py
@@ -1,4 +1,5 @@
 import warnings
+from pathlib import Path
 import dill as pickle
 import pandas as pd
 from sklearn.cluster import KMeans
@@ -270,3 +271,406 @@ def open_json(tec, load_path):
         raise Exception("There is no json data file for technology " + tec)
 
     return data
+
+
+def create_input_data_folder_template(base_path: Path | str) -> None:
+    """
+    This function creates the input data folder structure required to organize the input data to the model.
+    Note that the folder needs to already exist with a topology.json file in it that specifies the nodes, carriers,
+    timesteps, investement periods and the length of the investment period.
+
+    You can create an examplary json template with the function `create_topology_template`
+
+    :param str/Path base_path: path to folder
+    """
+    # Convert to Path
+    if isinstance(base_path, str):
+        base_path = Path(base_path)
+
+    # Read topology.json
+    with open(base_path / "topology.json") as json_file:
+        topology = json.load(json_file)
+
+    timesteps = pd.date_range(
+        start=topology["start_date"],
+        end=topology["end_date"],
+        freq=topology["resolution"],
+    )
+
+    # template_jsons:
+    networks = {"existing": [], "new": []}
+    technologies = {"existing": [], "new": []}
+    carrier_data = pd.DataFrame(
+        index=timesteps,
+        columns=[
+            "Demand",
+            "Import limit",
+            "Export limit",
+            "Import price",
+            "Export price",
+            "Import emission factor",
+            "Export emission factor",
+            "Generic production",
+        ],
+    )
+    climate_data = pd.DataFrame(
+        index=timesteps,
+        columns=["ghi", "dni", "dhi", "temp_air", "rh", "TECHNOLOGYNAME_hydro_inflow"],
+    )
+    carbon_cost = pd.DataFrame(index=timesteps, columns=["price", "subsidy"])
+    node_locations = pd.DataFrame(index=topology["nodes"], columns=["lon", "lat"])
+    node_locations.to_csv(base_path / "NodeLocations.csv", sep=";")
+
+    # Make folder structure
+    for investment_period in topology["investment_periods"]:
+        (base_path / investment_period).mkdir(parents=True, exist_ok=True)
+
+        # Networks
+        with open(base_path / investment_period / "Networks.json", "w") as f:
+            json.dump(networks, f, indent=4)
+        (base_path / investment_period / "network_data").mkdir(
+            parents=True, exist_ok=True
+        )
+
+        # Node data
+        (base_path / investment_period / "node_data").mkdir(parents=True, exist_ok=True)
+        for node in topology["nodes"]:
+            (base_path / investment_period / "node_data" / node / "carrier_data").mkdir(
+                parents=True, exist_ok=True
+            )
+            with open(
+                base_path
+                / investment_period
+                / "node_data"
+                / node
+                / "Technologies.json",
+                "w",
+            ) as f:
+                json.dump(technologies, f, indent=4)
+            for carrier in topology["carriers"]:
+                carrier_data.to_csv(
+                    base_path
+                    / investment_period
+                    / "node_data"
+                    / node
+                    / "carrier_data"
+                    / f"{carrier}.csv",
+                    sep=";",
+                )
+            climate_data.to_csv(
+                base_path / investment_period / "node_data" / node / "ClimateData.csv",
+                sep=";",
+            )
+            carbon_cost.to_csv(
+                base_path / investment_period / "node_data" / node / "CarbonCost.csv",
+                sep=";",
+            )
+            (
+                base_path / investment_period / "node_data" / node / "technology_data"
+            ).mkdir(parents=True, exist_ok=True)
+
+
+def create_optimization_templates(path: Path | str) -> None:
+    """
+    Creates an examplary topology json file in the specified path.
+
+    :param str/Path path: path to folder to create topology.json
+    """
+    if isinstance(path, str):
+        path = Path(path)
+
+    topology_template = {
+        "nodes": ["node1", "node2"],
+        "carriers": ["electricity", "hydrogen"],
+        "investment_periods": ["period1", "period2"],
+        "start_date": "2022-01-01 00:00",
+        "end_date": "2022-12-31 00:00",
+        "resolution": "1h",
+        "investment_period_length": 1,
+    }
+
+    configuration_template = {
+        "optimization": {
+            "objective": {
+                "description": "String specifying the objective/type of optimization.",
+                "options": [
+                    "costs",
+                    "emissions_pos",
+                    "emissions_net",
+                    "emissions_minC",
+                    "costs_emissionlimit",
+                    "pareto",
+                ],
+                "default": "costs",
+            },
+            "monte_carlo": {
+                "on": {
+                    "description": "Turn Monte Carlo simulation on.",
+                    "options": [0, 1],
+                    "default": 0,
+                },
+                "sd": {
+                    "description": "Value defining the range in which variables are varied in Monte Carlo simulations (defined as the standard deviation of the original value).",
+                    "default": 0.2,
+                },
+                "N": {
+                    "description": "Number of Monte Carlo simulations.",
+                    "default": 100,
+                },
+                "on_what": {
+                    "description": "List: Defines component to vary.",
+                    "options": ["Technologies", "ImportPrices", "ExportPrices"],
+                    "default": "Technologies",
+                },
+            },
+            "pareto_points": {"description": "Number of Pareto points.", "default": 5},
+            "timestaging": {
+                "description": "Defines number of timesteps that are averaged (0 = off).",
+                "default": 0,
+            },
+            "typicaldays": {
+                "N": {
+                    "description": "Determines number of typical days (0 = off).",
+                    "default": 0,
+                },
+                "method": {
+                    "description": "Determine method used for modeling technologies with typical days.",
+                    "options": [2],
+                    "default": 2,
+                },
+            },
+            "multiyear": {
+                "description": "Enable multiyear analysis, if turned off max time horizon is 1 year.",
+                "options": [0, 1],
+                "default": 0,
+            },
+        },
+        "solveroptions": {
+            "solver": {
+                "description": "String specifying the solver used.",
+                "default": "gurobi",
+            },
+            "mipgap": {"description": "Value to define MIP gap.", "default": 0.001},
+            "timelim": {
+                "description": "Value to define time limit in hours.",
+                "default": 10,
+            },
+            "threads": {
+                "description": "Value to define number of threads (default is maximum available).",
+                "default": 0,
+            },
+            "mipfocus": {
+                "description": "Modifies high level solution strategy.",
+                "options": [0, 1, 2, 3],
+                "default": 0,
+            },
+            "nodefilestart": {
+                "description": "Parameter to decide when nodes are compressed and written to disk.",
+                "default": 60,
+            },
+            "method": {
+                "description": "Defines algorithm used to solve continuous models.",
+                "options": [-1, 0, 1, 2, 3, 4, 5],
+                "default": -1,
+            },
+            "heuristics": {
+                "description": "Parameter to determine amount of time spent in MIP heuristics.",
+                "default": 0.05,
+            },
+            "presolve": {
+                "description": "Controls the presolve level.",
+                "options": [-1, 0, 1, 2],
+                "default": -1,
+            },
+            "branchdir": {
+                "description": "Determines which child node is explored first in the branch-and-cut.",
+                "options": [-1, 0, 1],
+                "default": 0,
+            },
+            "lpwarmstart": {
+                "description": "Controls whether and how warm start information is used for LP.",
+                "options": [0, 1, 2],
+                "default": 0,
+            },
+            "intfeastol": {
+                "description": "Value that determines the integer feasibility tolerance.",
+                "default": 1e-05,
+            },
+            "feastol": {
+                "description": "Value that determines feasibility for all constraints.",
+                "default": 1e-06,
+            },
+            "numericfocus": {
+                "description": "Degree of which Gurobi tries to detect and manage numeric issues.",
+                "options": [0, 1, 2, 3],
+                "default": 0,
+            },
+            "cuts": {
+                "description": "Setting defining the aggressiveness of the global cut.",
+                "options": [-1, 0, 1, 2, 3],
+                "default": -1,
+            },
+        },
+        "reporting": {
+            "save_detailed": {
+                "description": "Setting to select how the results are saved. When turned off only the summary is saved.",
+                "options": [0, 1],
+                "default": 1,
+            },
+            "save_path": {
+                "description": "Option to define the save path.",
+                "default": "./userData/",
+            },
+            "case_name": {
+                "description": "Option to define a case study name that is added to the results folder name.",
+                "default": -1,
+            },
+            "write_solution_diagnostics": {
+                "description": "If 1, writes solution quality, if 2 also writes pyomo to Gurobi variable map and constraint map to file.",
+                "options": [0, 1, 2],
+                "default": 0,
+            },
+        },
+        "energybalance": {
+            "violation": {
+                "description": "Determines the energy balance violation price (-1 is no violation allowed).",
+                "default": -1,
+            },
+            "copperplate": {
+                "description": "Determines if a copperplate approach is used.",
+                "options": [0, 1],
+                "default": 0,
+            },
+        },
+        "economic": {
+            "global_discountrate": {
+                "description": "Determines if and which global discount rate is used. This holds for the CAPEX of all technologies and networks.",
+                "default": -1,
+            },
+            "global_simple_capex_model": {
+                "description": "Determines if the CAPEX model of technologies is set to 1 for all technologies.",
+                "options": [0, 1],
+                "default": 0,
+            },
+        },
+        "performance": {
+            "dynamics": {
+                "description": "Determines if dynamics are used.",
+                "options": [0, 1],
+                "default": 0,
+            }
+        },
+        "scaling": {
+            "scaling": {
+                "description": "Determines if the model is scaled. If 1, it uses global and component specific scaling factors.",
+                "options": [0, 1],
+                "default": 0,
+            },
+            "scaling_factors": {
+                "energy_vars": {
+                    "description": "Scaling factor used for all energy variables.",
+                    "default": 0.001,
+                },
+                "cost_vars": {
+                    "description": "Scaling factor used for all cost variables.",
+                    "default": 0.001,
+                },
+                "objective": {
+                    "description": "Scaling factor used for the objective function.",
+                    "default": 1,
+                },
+            },
+        },
+    }
+
+    with open(path / "topology.json", "w") as f:
+        json.dump(topology_template, f, indent=4)
+    with open(path / "config_optimization.json", "w") as f:
+        json.dump(configuration_template, f, indent=4)
+
+
+def check_input_data_consistency(path: Path | str) -> None:
+    """
+    Checks if the topology is consistent with the input data.
+
+    :param str/Path node: node as specified in the topology
+    """
+
+    def check_path_existance(path: Path, error_message: str) -> None:
+        if not os.path.exists(path):
+            raise Exception(error_message)
+
+    # Convert to Path
+    if isinstance(path, str):
+        path = Path(path)
+
+    # Read topology
+    with open(path / "topology.json") as json_file:
+        topology = json.load(json_file)
+
+    for investment_period in topology["investment_periods"]:
+
+        # Check investment periods
+        check_path = path / investment_period
+        check_path_existance(
+            check_path,
+            f"The investment period {investment_period} is missing in {check_path}",
+        )
+
+        # Check if all networks have a json file
+        check_path_existance(
+            check_path / "Networks.json",
+            f"A Network.json file is missing in {check_path}",
+        )
+        with open(check_path / "Networks.json") as json_file:
+            networks = json.load(json_file)
+        networks = set(networks["existing"] + networks["new"])
+        for network in networks:
+            check_path_existance(
+                check_path / "network_data" / (network + ".json"),
+                f"A json file for {network} is missing in {check_path / 'network_data'}",
+            )
+
+        for node in topology["nodes"]:
+
+            # Check nodes
+            check_node_path = path / investment_period / "node_data" / node
+            check_path_existance(
+                check_node_path, f"The node {node} is missing in {check_node_path}"
+            )
+
+            # Check if all files are there
+            check_path_existance(
+                check_node_path / "ClimateData.csv",
+                f"ClimateData.csv is missing in {check_node_path}",
+            )
+            check_path_existance(
+                check_node_path / "CarbonCost.csv",
+                f"CarbonCost.csv is missing in {check_node_path}",
+            )
+            check_path_existance(
+                check_node_path / "Technologies.json",
+                f"Technologies.json is missing in {check_node_path}",
+            )
+
+            # Check if all technologies have a json file
+            with open(check_node_path / "Technologies.json") as json_file:
+                technologies_at_node = json.load(json_file)
+            technologies_at_node = set(
+                technologies_at_node["existing"] + technologies_at_node["new"]
+            )
+            for technology in technologies_at_node:
+                check_path_existance(
+                    check_node_path / "technology_data" / (technology + ".json"),
+                    f"A json file for {technology} is missing in {check_node_path / 'technology_data'}",
+                )
+                # TODO: Check if carriers are in carrier set
+
+            # Check if all carriers are there
+            for carrier in topology["carriers"]:
+                check_path_existance(
+                    check_node_path / "carrier_data" / (carrier + ".csv"),
+                    f"Data for carrier {carrier} is missing in {check_node_path}",
+                )
+
+    print("Input data folder has been checked successfully - no errors occurred.")
