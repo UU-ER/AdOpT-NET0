681b023debff4c05c2d88365590f7d8599117299
Luca <l.bertoni@uu.nl>
2024-04-26 16:49:19 +0200

    Case studies coded

--- caseStudies/Household/selectDemandProfile.py
+++ caseStudies/Household/selectDemandProfile.py
@@ -6,9 +6,9 @@ def select_one_demand_profile(csv_file):
     with open(csv_file, 'r') as file:
         csv_reader = csv.reader(file)
         for row in csv_reader:
-            if len(row) > 1:  # Check if the row has at least two columns
+            if len(row) > 5:  # Check if the row has at least two columns
                 try:
-                    value = float(row[1])  # Convert to float
+                    value = float(row[5])  # Convert to float
                     BE_demand_profile.append(value)  # Append to list
                 except ValueError:
                     pass  # Skip invalid values
@@ -17,11 +17,28 @@ def select_one_demand_profile(csv_file):
 csv_file = "TotalDemand_NT_2030_1995.csv"  # Replace with the path to your CSV file
 BE_demand_profile = select_one_demand_profile(csv_file)
 
-householdAnnualDemand = 3  # MWh
+householdAnnualDemand = 4  # MWh
 total_demand = np.sum(BE_demand_profile)
 rescale_factor = householdAnnualDemand / total_demand
 
 # Rescale the demand profile
 household_demand_profile = [rescale_factor * demand for demand in BE_demand_profile]
-a =1
+
+# Stretch the values
+min_demand = np.min(household_demand_profile)
+max_demand = np.max(household_demand_profile)
+average_demand = np.mean(household_demand_profile)
+
+stretched_demand_profile = 0.0015 * (household_demand_profile - average_demand)/max_demand + average_demand
+
+print(np.min(stretched_demand_profile))
+print(np.max(stretched_demand_profile))
+
+# Save the household_demand_profile to a CSV file
+output_csv_file = "el_demand.csv"
+with open(output_csv_file, 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    csv_writer.writerow(["Demand"])
+    for demand_value in stretched_demand_profile:
+        csv_writer.writerow([demand_value])
 
