5b8e98f6105a9aaef3c1911b0da9bca6d45529ab
Jean <j.f.wiegner@uu.nl>
2024-04-30 17:47:25 +0200

    Network recoded

--- src/components/networks/network.py
+++ src/components/networks/network.py
@@ -223,7 +223,9 @@ class Network(ModelComponent):
             # Use initial size
             self.size_max_arcs = self.size_initial
 
-    def construct_general_constraints(self, b_netw, energyhub):
+    def construct_general_constraints(
+        self, b_netw, data, set_nodes, set_t_full, set_t_clustered
+    ):
         r"""
         Adds a network as model block.
 
@@ -231,16 +233,18 @@ class Network(ModelComponent):
         :return: network model
         """
         # Data from energyhub
-        self.set_nodes = energyhub.model.set_nodes
-        self.set_t = energyhub.model.set_t_full
+        config = data["config"]
 
-        b_netw = self._define_possible_arcs(b_netw, energyhub)
+        self.set_nodes = set_nodes
+        self.set_t = set_t_full
+
+        b_netw = self._define_possible_arcs(b_netw, data)
 
         if self.performance_data["bidirectional"] == 1:
             b_netw = self._define_unique_arcs(b_netw)
 
         b_netw = self._define_size(b_netw)
-        b_netw = self._define_capex_parameters(b_netw, energyhub)
+        b_netw = self._define_capex_parameters(b_netw, data)
         b_netw = self._define_opex_parameters(b_netw)
         b_netw = self._define_emission_vars(b_netw)
         b_netw = self._define_network_characteristics(b_netw)
@@ -379,7 +383,7 @@ class Network(ModelComponent):
 
         return model
 
-    def _define_possible_arcs(self, b_netw, energyhub):
+    def _define_possible_arcs(self, b_netw, data):
         """
         Define all possible arcs that have a connection
 
@@ -388,7 +392,6 @@ class Network(ModelComponent):
         - set_receives_from: Set of nodes for each node specifying receiving from nodes
         - set_sends_to: Set of nodes for each node specifying sending to nodes
         """
-        model = energyhub.model
         connection = copy.deepcopy(self.connection[:])
 
         def init_arcs_set(set):
@@ -404,14 +407,14 @@ class Network(ModelComponent):
                 if j == node:
                     yield i
 
-        b_netw.set_receives_from = Set(model.set_nodes, initialize=init_nodesIn)
+        b_netw.set_receives_from = Set(self.set_nodes, initialize=init_nodesIn)
 
         def init_nodesOut(set, node):
             for i, j in b_netw.set_arcs:
                 if i == node:
                     yield j
 
-        b_netw.set_sends_to = Set(model.set_nodes, initialize=init_nodesOut)
+        b_netw.set_sends_to = Set(self.set_nodes, initialize=init_nodesOut)
 
         return b_netw
 
@@ -470,7 +473,7 @@ class Network(ModelComponent):
                         )
         return b_netw
 
-    def _define_capex_parameters(self, b_netw, energyhub):
+    def _define_capex_parameters(self, b_netw, data):
         """
         Defines variables and parameters related to technology capex.
 
@@ -481,13 +484,12 @@ class Network(ModelComponent):
         - total capex for network
         """
 
-        configuration = energyhub.configuration
-
+        config = data["config"]
         economics = self.economics
 
         # CHECK FOR GLOBAL ECONOMIC OPTIONS
         discount_rate = set_discount_rate(config, economics)
-        fraction_of_year_modelled = energyhub.topology.fraction_of_year_modelled
+        fraction_of_year_modelled = data["topology"]["fraction_of_year_modelled"]
 
         # CAPEX
         annualization_factor = annualize(
