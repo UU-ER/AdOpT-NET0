67b83edfa3c8d1962026f45a48b57ee711f5f4f4
Jean <j.f.wiegner@uu.nl>
2024-04-30 10:13:06 +0200

    - test global cost balance

--- src/test/test_balances.py
+++ src/test/test_balances.py
@@ -13,7 +13,14 @@ from pyomo.environ import (
 from src.test.utilities import create_patched_datahandle
 from src.components.technologies.technology import Technology
 from src.energyhub import EnergyHub
-from src.model_construction.construct_balances import *
+from src.model_construction.construct_balances import (
+    construct_global_balance,
+    construct_global_energybalance,
+    construct_emission_balance,
+    construct_nodal_energybalance,
+    construct_network_constraints,
+    construct_system_cost,
+)
 
 
 def construct_model(dh):
@@ -27,7 +34,7 @@ def construct_model(dh):
 
 
 def solve_model(m):
-    solver = SolverFactory("glpk")
+    solver = SolverFactory("gurobi")
     solution = solver.solve(m)
     termination_condition = solution.solver.termination_condition
     return termination_condition
@@ -198,3 +205,53 @@ def test_model_emission_balance():
 
 
 # Todo: Test system costs
+def test_model_cost_balance():
+    """
+    Construct a mock technology model for testing
+
+    :param Technology tec: Technology object.
+    :param int nr_timesteps: Number of timesteps to create climate data for
+    :return ConcreteModel m: Pyomo Concrete Model
+    """
+    nr_timesteps = 1
+
+    dh = create_patched_datahandle(nr_timesteps)
+    config = {"energybalance": {"violation": {"value": 0}, "copperplate": {"value": 0}}}
+    period = dh.topology["investment_periods"][0]
+    node = dh.topology["nodes"][0]
+    carrier = dh.topology["carriers"][0]
+
+    # INFEASIBILITY CASE
+    dh.time_series["full"].loc[:, (period, node, "CarrierData", carrier, "Demand")] = 1
+    dh.time_series["full"].loc[
+        :, (period, node, "CarrierData", carrier, "Import limit")
+    ] = 1
+    dh.time_series["full"].loc[
+        :, (period, node, "CarrierData", carrier, "Import price")
+    ] = 1
+
+    m = construct_model(dh)
+    m = construct_network_constraints(m)
+    m = construct_global_energybalance(m, config)
+    m = construct_system_cost(m, config)
+    m = construct_global_balance(m)
+
+    m.test_const_system_costs = Constraint(expr=m.var_npv == 2)
+
+    termination_condition = solve_model(m)
+    assert termination_condition == TerminationCondition.infeasible
+
+    # FEASIBILITY CASE
+    # Through violation
+
+    m = construct_model(dh)
+    m = construct_network_constraints(m)
+    m = construct_global_energybalance(m, config)
+    m = construct_system_cost(m, config)
+    m = construct_global_balance(m)
+
+    termination_condition = solve_model(m)
+    assert termination_condition == TerminationCondition.optimal
+    assert m.periods[period].node_blocks[node].var_import_flow[1, carrier].value == 1
+    assert m.periods[period].var_cost_imports.value == 1
+    assert m.var_npv.value == 1
