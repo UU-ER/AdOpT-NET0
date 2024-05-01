405ea7b1560715db3bc382ee42bb46e1291985f7
6145795 <inge.ossentjuk@gmail.com>
2024-05-01 17:29:01 +0200

    Docstrings of src\result_management updated according to correct format.

--- src/result_management/read_results.py
+++ src/result_management/read_results.py
@@ -1,12 +1,16 @@
 import h5py
 import pandas as pd
+from pathlib import Path
 
 
-def print_h5_tree(file_path):
+def print_h5_tree(file_path: Path | str):
     """
-    Function to print all groups and datasets of a h5 file
+    Function to print the structure of a h5 file
 
-    :param file_path: Path to H5 File
+    The structure of a h5 file is a tree structure: the h5 file is the root group, from which all groups stem,
+    and datasets are the leaves contained within a group.
+
+    :param Path, str file_path: Path to H5 File
     """
     with h5py.File(file_path, "r") as hdf_file:
 
@@ -18,12 +22,17 @@ def print_h5_tree(file_path):
         hdf_file.visititems(print_attrs)
 
 
-def extract_datasets_from_h5group(group, prefix=()):
+def extract_datasets_from_h5group(group: h5py.Group, prefix: tuple = ()) -> pd.DataFrame:
     """
+    Extracts datasets from a group within an h5 file
+
     Gets all datasets from a group of an h5 file and writes it to a multi-index dataframe
 
-    :param group: group of h5 file
+    :param h5py.Group group: of h5 file
+    :param tuple prefix: required to search through the structure of the h5 tree if there are multiple subgroups in the
+     group you specified, empty by default meaning it starts searching from the group specified.
     :return: dataframe containing all datasets in group
+    :rtype: pd.DataFrame
     """
     data = {}
     for key, value in group.items():
@@ -40,12 +49,15 @@ def extract_datasets_from_h5group(group, prefix=()):
     return df
 
 
-def extract_dataset_from_h5(dataset):
+def extract_dataset_from_h5(dataset: h5py.Dataset) -> list:
     """
-    Gets dataset from an h5 file
+    Extracts values from a dataset within an h5 file
 
-    :param group: group of h5 file
-    :return: dataframe containing all datasets in group
+    Gets all values of a dataset in an h5 file and writes it to a list.
+
+    :param h5py.Dataset dataset: dataset within an h5 file
+    :return: list of all values in a dataset
+    :rtype: list
     """
     data = [item.decode("utf-8") for item in dataset]
 
