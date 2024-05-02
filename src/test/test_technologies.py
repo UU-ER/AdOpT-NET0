cf02aff8e8f71472977531c5e9109b99eac1fa0f
Jean <j.f.wiegner@uu.nl>
2024-05-02 11:13:19 +0200

    DAC test finished

--- src/test/test_technologies.py
+++ src/test/test_technologies.py
@@ -726,8 +726,29 @@ def test_dynamics_fast(request):
 
 
 def test_dac(request):
+    """
+    tests DAC Adsorption
+    """
     time_steps = 1
     technology = "TestTec_DAC_adsorption"
     tec = define_technology(
         technology, time_steps, request.config.technology_data_folder_path
     )
+
+    # INFEASIBILITY CASES
+    model = construct_tec_model(tec, nr_timesteps=time_steps)
+    model = generate_output_constraint(model, [1])
+    model.test_const_input = Constraint(expr=model.var_input[1, "electricity"] == 0)
+
+    termination = run_model(model)
+    assert termination == TerminationCondition.infeasibleOrUnbounded
+
+    # FEASIBILITY CASES
+    model = construct_tec_model(tec, nr_timesteps=time_steps)
+    model = generate_output_constraint(model, [1])
+
+    termination = run_model(model)
+    assert termination == TerminationCondition.optimal
+    assert model.var_input[1, "heat"].value > 0.1
+    assert model.var_input[1, "electricity"].value > 0.01
+    assert model.var_size.value > 1
