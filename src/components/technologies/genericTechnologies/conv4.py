9b382cb864067ebb9d27408631466eb71dad23d8
IngeOssentjuk <124692905+IngeOssentjuk@users.noreply.github.com>
2024-05-08 10:30:09 +0200

    Updated docstrings conv4.py

--- src/components/technologies/genericTechnologies/conv4.py
+++ src/components/technologies/genericTechnologies/conv4.py
@@ -3,6 +3,7 @@ from pyomo.gdp import *
 import copy
 from warnings import warn
 import numpy as np
+import pandas as pd
 
 from ..utilities import FittedPerformance
 from ..technology import Technology
@@ -10,30 +11,37 @@ from ..technology import Technology
 
 class Conv4(Technology):
     """
-    This technology type resembles a technology with fixed output ratios and no inputs, i.e. :math:`output_{car} \leq S`
+    This technology type resembles a technology with fixed output ratios and no inputs, i.e., :math:`output_{car} \leq S`.
+    This technology is useful for modelling a technology for which you do not care about the inputs, i.e., you do not
+    wish to construct and solve an energy balance for the input carriers.
     Two different performance function fits are possible.
 
     **Constraint declarations:**
+
+    For all performance function types, the following constraints hold:
 
     - Size constraints are formulated on the output.
 
       .. math::
          Output_{t, maincarrier} \leq S
 
-    - The ratios of outputs for all performance function types are fixed and given as:
+    - The ratios of outputs are fixed and given as:
 
       .. math::
         Output_{t, car} = {\\phi}_{car} * Output_{t, maincarrier}
 
-    - ``performance_function_type == 1``: No further constraints:
+    For type 1, there are no further constraints on the performance of the technology. So, for
+    ``performance_function_type == 1`` only the above constraints hold.
 
-    - ``performance_function_type == 2``: Minimal partload (makes big-m transformation required). If the
-      technology is in on, it holds:
+    For type 2, a minimum part load can be specified (requiring a big-m transformation for the solving). So, for
+    ``performance_function_type == 2``, the following constraints hold:
+
+    - When the technology is on:
 
       .. math::
         Output_{maincarrier} \geq Output_{min} * S
 
-      If the technology is off, output is set to 0:
+    - When the technology is off, output is set to 0:
 
       .. math::
          Output_{t, car} = 0
@@ -46,13 +54,17 @@ class Conv4(Technology):
 
         self.main_car = self.performance_data["main_output_carrier"]
 
-    def fit_technology_performance(self, climate_data, location):
+    def fit_technology_performance(self, climate_data: pd.DataFrame, location: dict):
         """
         Fits conversion technology type 4 and returns fitted parameters as a dict
 
-        :param node_data: contains data on demand, climate data, etc.
-        :param performance_data: contains X and y data of technology performance
-        :param performance_function_type: options for type of performance function (linear, piecewise,...)
+        The performance data for a specific location (node) as specified in the JSON (containing X,Y data of tech
+        performance) is fitted.
+
+        :param pd.DataFrame climate_data: a dataframe containing all climate data for the respective node at which the
+        tec is installed.
+        :param dict location: the coordinates of the node, needed because the method is called from a higher level than
+        the node level.
         """
 
         self.fitted_performance.bounds["output"][
@@ -69,13 +81,16 @@ class Conv4(Technology):
                     * self.performance_data["output_ratios"][car]
                 )
 
-    def construct_tech_model(self, b_tec, data, set_t, set_t_clustered):
+    def construct_tech_model(self, b_tec: Block, data: pd.DataFrame, set_t: Set, set_t_clustered: Set) -> Block:
         """
         Adds constraints to technology blocks for tec_type CONV4
 
-        :param obj b_tec: technology block
-        :param Energyhub energyhub: energyhub instance
-        :return: technology block
+        :param Block b_tec: technology block
+        :param pd.DataFrame data: dataframe of all input data
+        :param Set set_t: set of all time steps
+        :param Set set_t_clustered: set of all time steps when using the clustering algorithm
+        :return: technology block (b_tec)
+        :rtype: Block
         """
         super(Conv4, self).construct_tech_model(b_tec, data, set_t, set_t_clustered)
 
@@ -87,6 +102,7 @@ class Conv4(Technology):
         for car in self.performance_data["output_ratios"]:
             phi[car] = self.performance_data["output_ratios"][car]
 
+        # add additional constraints for performance type 2 (min. part load)
         if performance_function_type == 2:
             b_tec = self._performance_function_type_2(b_tec)
 
@@ -105,7 +121,7 @@ class Conv4(Technology):
             self.set_t, b_tec.set_output_carriers, rule=init_output_output
         )
 
-        # size constraint based main carrier output
+        # size constraint based on main carrier output
         def init_size_constraint(const, t):
             return self.output[t, self.main_car] <= b_tec.var_size * rated_power
 
@@ -113,11 +129,16 @@ class Conv4(Technology):
 
         return b_tec
 
-    def _performance_function_type_2(self, b_tec):
+    def _performance_function_type_2(self, b_tec: Block) -> Block:
         """
-        Linear, minimal partload
-        :param b_tec: technology block
-        :return: technology block
+        Sets the minimum part load constraint for a tec based on tec_type CONV4 with performance type 2.
+
+        Type 2 is a performance including a minimum part load. The technology can either be switched off, or it has to
+        operate beyond the minimum part load point.
+
+        :param Block b_tec: technology block
+        :return: the technology block (b_tec)
+        :rtype: Block
         """
         # Transformation required
         self.big_m_transformation_required = 1
