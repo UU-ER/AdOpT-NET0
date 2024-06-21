ffd0e5351284e3b4143c0728d39c8400c8f5f109
Jean <j.f.wiegner@uu.nl>
2024-06-21 13:42:22 +0200

    Bug fix in network.py: - for integer size of networks the bounds of the capex was not correct (it neglected the rated capacity)

--- adopt_net0/components/networks/network.py
+++ adopt_net0/components/networks/network.py
@@ -688,13 +688,17 @@ class Network(ModelComponent):
         :param b_netw: pyomo network block
         :return: pyomo arc block
         """
+        rated_capacity = self.input_parameters.rated_power
 
         def calculate_max_capex():
             max_capex = (
                 b_netw.para_capex_gamma1
-                + b_netw.para_capex_gamma2 * b_arc.para_size_max
+                + b_netw.para_capex_gamma2 * b_arc.para_size_max * rated_capacity
                 + b_netw.para_capex_gamma3 * b_arc.distance
-                + b_netw.para_capex_gamma4 * b_arc.para_size_max * b_arc.distance
+                + b_netw.para_capex_gamma4
+                * b_arc.para_size_max
+                * rated_capacity
+                * b_arc.distance
             )
             return (0, max_capex)
 
